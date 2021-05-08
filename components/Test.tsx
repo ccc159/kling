9eab57aebbd1c0f9e11f1c8aeb398e69300713bc
Jingcheng Chen <mail@chenjingcheng.com>
2021-05-08 13:11:30 +0200

    added test phase 2

--- components/Test.tsx
+++ components/Test.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, Pressable, View, Modal } from 'react-native';
 import { useInterval } from '../hooks/useInterval';
 import { ITask } from '../store/task';
@@ -22,9 +22,12 @@ export const Test = ({ test, task }: ITestProps) => {
     console.log('pressed');
   }
 
+  // console.log(test);
+
   if (test.result) return <TestResult {...{ task, test }} />;
 
   if (!test.timestamp.phase1) return <TestPhase1 {...{ task, test }} />;
+  else if (!test.timestamp.phase2) return <TestPhase2 {...{ task, test }} />;
 
   return (
     <View style={Styles.circleStyle}>
@@ -37,10 +40,22 @@ export const Test = ({ test, task }: ITestProps) => {
 };
 
 const TestPhase1 = ({ test, task }: ITestProps) => {
+  const readyMinutes = 1;
+  const expireMinutes = 3;
+  const fromDate = new Date(test.timestamp.start!);
   const [showModal, setShowModal] = useState<boolean>(false);
+
+  const isReady = IsTimeUp(readyMinutes, fromDate);
+  const isExpired = IsTimeUp(expireMinutes, fromDate);
+
+  useEffect(() => {
+    if (!isExpired) return;
+    const updateTest: ITest = { ...test, result: Result.Invalid };
+    task.UpdateTest(updateTest);
+  }, [isExpired]);
+
   function onPress() {
-    const isTimeUp = IsTimeUp(1, new Date(test.timestamp.start!));
-    if (!isTimeUp) return;
+    if (!isReady) return;
     setShowModal(true);
   }
 
@@ -65,9 +80,58 @@ const TestPhase1 = ({ test, task }: ITestProps) => {
           </View>
         </View>
       </Modal>
-      <Timer color='black' countDownMinutes={1} from={test.timestamp.start!} />
+      {!isReady && <Timer color='black' countDownMinutes={readyMinutes} from={fromDate} />}
       <Pressable onPress={onPress}>
-        <Text style={styles.itemText}>{test.tester}</Text>
+        <Text style={styles.itemText}>{isReady ? 'ready' : test.tester}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const TestPhase2 = ({ test, task }: ITestProps) => {
+  const readyMinutes = 1.5;
+  const expireMinutes = 3;
+  const fromDate = new Date(test.timestamp.phase1!);
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const isReady = IsTimeUp(readyMinutes, fromDate);
+  const isExpired = IsTimeUp(expireMinutes, fromDate);
+
+  useEffect(() => {
+    if (!isExpired) return;
+    const updateTest: ITest = { ...test, result: Result.Invalid };
+    task.UpdateTest(updateTest);
+  }, [isExpired]);
+
+  function onPress() {
+    if (!isReady) return;
+    setShowModal(true);
+  }
+
+  function closeModal() {
+    setShowModal(false);
+  }
+
+  function startNextPhase() {
+    const updateTest: ITest = { ...test, timestamp: { ...test.timestamp, phase1: new Date() } };
+    task.UpdateTest(updateTest);
+    closeModal();
+  }
+
+  return (
+    <View style={Styles.circleStyle}>
+      <Modal animationType='fade' transparent={true} visible={showModal}>
+        <View style={styles.centeredView}>
+          <View style={Styles.boxStyle}>
+            <Text>Are you ready for next phase?</Text>
+            <MyButton title={'No'} onPress={closeModal} />
+            <MyButton title={'Ready'} onPress={startNextPhase} />
+          </View>
+        </View>
+      </Modal>
+      {!isReady && <Timer color='black' countDownMinutes={readyMinutes} from={fromDate} />}
+      <Pressable onPress={onPress}>
+        <Text style={styles.itemText}>{isReady ? 'ready' : test.tester}</Text>
       </Pressable>
     </View>
   );
