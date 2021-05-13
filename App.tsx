58c89de6459f6a8acfa66aab2a8637dd791e7ec1
Jingcheng Chen <mail@chenjingcheng.com>
2021-05-13 17:15:26 +0200

    keep the app awake

--- App.tsx
+++ App.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Dashboard } from './dashboard';
 import { ContextProvider } from './store';
+import { useKeepAwake } from 'expo-keep-awake';
 
 export default function App() {
+  useKeepAwake();
   return (
     <ContextProvider>
       <View style={styles.container}>
